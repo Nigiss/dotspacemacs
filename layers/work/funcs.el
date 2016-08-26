fd070d8e161f24f07b80255cdb08de48c9320cfe
yujun <yujun1@xiaomi.com>
2016-08-26 18:39:16 +0800

    opt: update work config.

--- layers/work/funcs.el
+++ layers/work/funcs.el
@@ -1,22 +1,20 @@
 ;; web or store-hybrid
 (defun synelics-work/server-start ()
   (interactive)
-  (synelics-work||exec-shell "bash run.sh"))
+  (synelics-work||exec-command-with-shell "bash run.sh"))
 
 ;; phone test
 (defun synelics-work/phone-test ()
   "Test for phone module"
   (interactive)
-  (split-window-below-and-focus)
-  (ansi-term "/usr/bin/zsh")
-  (term-send-raw-string "~/kits/bin/test-phone-v3\n")
+  (synelics-work//exec-command-with-term "~/kits/bin/test-phone-v3")
   (evil-window-up 1))
 
 ;; phone cmd
 (defun synelics-work/phone-dev ()
   "Sync to origin/master"
   (interactive)
-  (synelics-work||exec-shell (concat (synelics-work//hybrid-command "dev" "phone")
+  (synelics-work||exec-command-with-shell (concat (synelics-work//hybrid-command "dev" "phone")
                                      " "
                                      (read-string "dev type: ")
                                      " "
@@ -26,32 +24,40 @@
 (defun synelics-work/phone-sync ()
   "Sync to origin/master"
   (interactive)
-  (synelics-work||exec-shell (synelics-work//hybrid-command "sync")))
+  (synelics-work||exec-command-with-shell (synelics-work//hybrid-command "sync")))
 
 (defun synelics-work/phone-hotfix ()
   "Sync to origin/master"
   (interactive)
-  (synelics-work||exec-shell (synelics-work//hybrid-command "hotfix") 'sync))
+  (synelics-work||exec-command-with-shell (synelics-work//hybrid-command "hotfix") 'sync))
 
 (defun synelics-work/phone-alpha ()
   "Sync to origin/master"
   (interactive)
-  (synelics-work||exec-shell (synelics-work//hybrid-command "alpha")))
+  (synelics-work||exec-command-with-shell (synelics-work//hybrid-command "alpha")))
 
 (defun synelics-work/phone-publish ()
   "Sync to origin/master"
   (interactive)
-  (synelics-work||exec-shell (synelics-work//hybrid-command "publish")))
+  (synelics-work||exec-command-with-shell (synelics-work//hybrid-command "publish")))
 
 
 ;;; helper
 (cl-defun synelics-work//hybrid-command (type &optional (project "phone"))
   (concat "bash cmd " type " " project))
 
-(defmacro synelics-work||exec-shell (cmd &optional sync)
+(defmacro synelics-work||exec-command-with-shell (cmd &optional sync)
   `(projectile-with-default-dir (projectile-project-root)
      (if ,sync
          (shell-command ,cmd)
+       (delete-other-windows)
        (split-window-below-and-focus)
        (shell)
        (comint-send-string "*shell*" (concat ,cmd "\n")))))
+
+
+(defun synelics-work//exec-command-with-term (cmd)
+  (delete-other-windows)
+  (split-window-below-and-focus)
+  (let ((term-buffer (multi-term)))
+    (term-send-string term-buffer (concat cmd "\n"))))
