eada28a0afaf488c11afd8b77302079bc48d8aa3
yujun <yujun1@xiaomi.com>
2016-08-26 18:39:56 +0800

    fix: shell and term mode key.

--- layers/synelics-shell/packages.el
+++ layers/synelics-shell/packages.el
@@ -11,10 +11,38 @@
 
 (setq synelics-shell-packages
       '(
+        term
+        (shell :location built-in)
         eshell
         xterm-color
         with-editor
         ))
+
+(defun synelics-shell/post-init-term ()
+  (use-package term
+    :init
+    (progn
+      (evil-define-key 'normal term-raw-map "p" 'term-paste)
+      (evil-define-key 'normal term-raw-map (kbd "RET") 'term-send-return)
+
+      (evil-define-key 'insert term-raw-map (kbd "C-w") 'term-send-raw)
+      (evil-define-key 'insert term-raw-map (kbd "C-r") 'term-send-raw)
+      (evil-define-key 'insert term-raw-map (kbd "C-s") 'term-send-raw)
+      (evil-define-key 'insert term-raw-map (kbd "M-DEL") 'term-send-raw)
+      (evil-define-key 'insert term-raw-map (kbd "TAB") 'term-send-raw)
+      (evil-define-key 'insert term-raw-map (kbd "DEL") 'term-send-raw))))
+
+(defun shell/post-init-shell ()
+  (add-hook 'shell-mode-hook
+            (lambda ()
+              (let ((map company-active-map))
+                ;; FIXME should set local key
+                (define-key map (kbd "RET") (lambda ()
+                                              (interactive)
+                                              (condition-case nil
+                                                  (comint-send-input)
+                                                (error
+                                                 (newline-and-indent)))))))))
 
 (defun synelics-shell/post-init-eshell ()
   (use-package eshell
