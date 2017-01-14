319d9d531963b5f3f8c2ee043a247cdf7f427150
yujun <yjun.ncepu@gmail.com>
2017-01-14 10:54:14 +0800

    Css mode color support syntax-color

--- layers/synelics-html/packages.el
+++ layers/synelics-html/packages.el
@@ -31,6 +31,32 @@
     :defer t
     :config
     (progn
+      (defun synelics-html//syntax-color ()
+        "Syntax color text of the form 「#ff1100」 and 「#abc」 in current buffer.
+URL `http://ergoemacs.org/emacs/emacs_CSS_colors.html'
+Version 2016-07-04"
+        (interactive)
+        (font-lock-add-keywords
+         nil
+         '(("#[ABCDEFabcdef[:digit:]]\\{3\\}"
+            (0 (put-text-property
+                (match-beginning 0)
+                (match-end 0)
+                'face (list :background
+                            (let* (
+                                   (ms (match-string-no-properties 0))
+                                   (r (substring ms 1 2))
+                                   (g (substring ms 2 3))
+                                   (b (substring ms 3 4)))
+                              (concat "#" r r g g b b))))))
+           ("#[ABCDEFabcdef[:digit:]]\\{6\\}"
+            (0 (put-text-property
+                (match-beginning 0)
+                (match-end 0)
+                'face (list :background (match-string-no-properties 0)))))))
+        (font-lock-fontify-buffer))
+
+      (add-hook 'css-mode-hook 'synelics-html//syntax-color)
       (add-hook 'css-mode-hook 'paredit-mode)
       (add-hook 'css-mode-hook
                 (lambda ()
