4b23d7aa29a5434cb46ea522cf646d714de09234
yujun <yjun.ncepu@gmail.com>
2017-01-19 15:30:49 +0800

    Set osx hot key.

---
+++ layers/synelics-keybind/keybindings.el
@@ -0,0 +1,19 @@
+;;; config.el --- OSX Layer keybindings File for Spacemacs
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(when (spacemacs/system-is-mac)
+  ;; this is only applicable to GUI mode
+  (when (display-graphic-p)
+    ;; Keybindings
+    (global-set-key (kbd "s-m") 'counsel-recentf)
+    (global-set-key (kbd "s-f") 'counsel-find-file)
+    (global-set-key (kbd "s-p") 'projectile-find-file)
+    (global-set-key (kbd "s-x") 'counsel-M-x)))
