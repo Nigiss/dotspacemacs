3658bf41d7c892fc047ef8633b7210b649453416
yujun <yjun.ncepu@gmail.com>
2017-09-03 10:28:53 +0800

    General config.

--- layers/synelics-keybind/keybindings.el
+++ layers/synelics-keybind/keybindings.el
@@ -13,6 +13,7 @@
   ;; this is only applicable to GUI mode
   (when (display-graphic-p)
     ;; Keybindings
+    (global-set-key (kbd "H-j") 'evil-avy-goto-char-2)
     (global-set-key (kbd "H-s") 'save-buffer)
     (global-set-key (kbd "H-g") 'goto-line)
     (global-set-key (kbd "H-k") 'kill-this-buffer)
@@ -21,3 +22,8 @@
     (global-set-key (kbd "H-f") 'counsel-find-file)
     (global-set-key (kbd "H-p") 'projectile-find-file)
     (global-set-key (kbd "H-x") 'counsel-M-x)))
+
+(define-key minibuffer-local-shell-command-map (kbd "C-p") 'previous-line-or-history-element)
+(define-key minibuffer-local-shell-command-map (kbd "C-n") 'next-line-or-history-element)
+(define-key minibuffer-local-shell-command-map (kbd "C-k") 'previous-line-or-history-element)
+(define-key minibuffer-local-shell-command-map (kbd "C-j") 'next-line-or-history-element)
