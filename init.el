bf72047f329fde4815e00dc9d62d3de7ec783061
yujun <yjun.ncepu@gmail.com>
2018-09-10 08:05:48 +0800

    fix: update .emacs.d

--- init.el
+++ init.el
@@ -368,7 +368,7 @@ values."
 It is called immediately after `dotspacemacs/init'.  You are free to put almost
 any user code here.  The exception is org related code, which should be placed
 in `dotspacemacs/user-config'."
-  (add-to-list 'configuration-layer--elpa-archives
+  (add-to-list 'configuration-layer-elpa-archives
                '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
   ;; https://github.com/syl20bnr/spacemacs/issues/2705
